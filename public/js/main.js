2fe1bbc9d8bc5a70d35aef13ededf674225ea673
hongnhungnguyen <hongnhungnguyen2602@gmail.com>
2014-06-11 23:49:50 +0700

    first add

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by HongNhung on 11/6/2014.
+ */
+
+function my_alert() {
+    window.alert('Hello, Nhung. Welcome back!' + ' You have ' + document.getElementsByClassName('footer-title').length + ' footer title');
+}
+
+my_alert();
